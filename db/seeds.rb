4b6b850bc8821602054fb1e33919fd1c5c6be4d1
spaseyhin <evgen.kalin99@gmail.com>
2022-08-04 14:45:18 +0400

    add validation and change vievs index

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,20 @@ Product.create!(
   description:%{Ruby is the fastest growing and most exciting dynamic language
 out there. If you need to get working programs delivered fast,
 you should add Ruby to your toolbox.},
- image_url: 'ruby.jpg',
+ image_url: 'https://static-cse.canva.com/blob/847064/29.jpg',
  price: 49.95)
+ Product.create!(
+   title: 'Programming Ruby 1.9 & 2.0',
+   description:%{Ruby is the fastest growing and most exciting dynamic language
+ out there. If you need to get working programs delivered fast,
+ you should add Ruby to your toolbox.},
+  image_url: 'https://bigpicture.ru/wp-content/uploads/2013/06/Capes04-800x500.jpg',
+  price: 49.95)
+
+  Product.create!(
+    title: 'Programming Ruby 1.9 & 2.0',
+    description:%{Ruby is the fastest growing and most exciting dynamic language
+  out there. If you need to get working programs delivered fast,
+  you should add Ruby to your toolbox.},
+   image_url: 'https://bigpicture.ru/wp-content/uploads/2015/11/nophotoshop29-800x532.jpg',
+   price: 49.95)
